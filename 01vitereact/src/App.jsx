1797bc1264029ca30e041100645a70e7c32f350c
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-18 11:41:52 +0530

    feat: update App and main components with dynamic rendering and improved custom render logic

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./Chai"
 
 function App() {
-  
+  const username = "chai aur code"
+
   return (
-    <Chai />
+    //<Chai />
     // <h1>Hi how are u?</h1> // this is not allowed because we can only return only tag in jsx
     // hence we wrap all the element in a <div> </div> and then return , this <div> become so common that now we use fragment <> </>
     // to return multiple tags enclosed within them
@@ -12,6 +13,13 @@ function App() {
    <h1></h1>
    <p></p>
   </> */
+
+  <>
+   <Chai />
+   <h1>chai aur react {username}</h1> {/* {username} is called "evaluated expression" i.e. it is not javascript it is the final outcome of the javascript*/}  
+   {/* <h1>chai aur react {if(true) username}</h1> // this is invaild because we must give evaluated expression. Reason? See in main.jsx file */}
+   <p>test para</p>
+  </>
   )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,53 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  )
+}
+
+// the above HTML syntax is parsed into somewhat below React syntax:
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit Google'
+}
+
+//SO question arises if we run the above react syntax directly we should get same result
+//Lets try to run it
+
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai user" //evaluated expression
+
+const reactElement2 = React.createElement( //lets make a element with predefine syntax for React elements
+  'a', // first para must be tag
+  {href: 'https://google.com', target: '_blank'},  //second para musst be object
+  'click me to visit google', // third para is text
+  anotherUser // using the evaluted expression // hence this must be evaluted expression as it appears directly on the browser and it is not evalauted
+             //afterall reactElement2 is a object and in object we dont pass if() else we only pass key: value that are evaluated already
+)
 
 createRoot(document.getElementById('root')).render( // starts the rendering directly// what is rendering?  
-  //<StrictMode> //optional
-    <App />
-  //</StrictMode>,
-)
+  
+    // <MyApp /> //it is a function that is been executed by this syntax
+    // MyApp() // the above can also be written directly as this because after all we are executing a js function
+    //<ReactElement/> but ReactElement is a object and not function so its execution should be not this way
+    // ReactElement //but this will also not run as we our running custom ReactElement using default render
+    
+    //anotherElement // this runs properly because we have written the anotherElement
+                // as expected by the default render
+    reactElement2
+
+)  
