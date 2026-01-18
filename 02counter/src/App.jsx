882a47e16ec121f4da2e8ad170da9ed2e83951b5
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-18 23:02:44 +0530

    feat: refactor App component to implement counter functionality with add and remove features

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,31 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// React controls UI updation
+//What are hooks?
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [counter,setCounter] = useState(15) //useState() hook is responsible for changing the state, this means this change is propogated in the UI, in the DOM 
+                // we can pass any default value , we have passed the initial value of the counter
+                //counter is a variable here and setCounter is a function that updates all the occurence of the counter variable
+  //let counter = 15
+
+  const addValue = () => {
+    //console.log("value added", Math.random())
+    //console.log("clicked", counter)
+    counter = counter + 1;
+    if(counter > 20) counter = 20;
+    setCounter(counter)
+    console.log("clicked", counter)
+  }
+
+  const removeValue = () => {
+    counter = counter - 1;
+    if(counter < 0) counter = 0;
+    setCounter(counter)
+    console.log("clicked", counter)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Chai aur React</h1>
+      <h3>Counter value : {counter}</h3>
+
+      <button onClick={addValue}>Add Value{counter}</button>
+      <br />
+      <button onClick={removeValue} >Remove Value{counter}</button>
+      <p>footer : {counter}</p>
     </>
   )
 }
