40cef14b18fd09ccc1d60197243a62fbffefff27
ShreyashDesai021 <dshreyash04@gmail.com>
2026-01-23 19:19:26 +0530

    feat: implement password copy functionality and enhance password generation logic

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect,useRef } from 'react'
 
 //const cachedFn = useCallback(fn, dependencies) useCallback is a React Hook that lets you cache a function definition between re-renders.
 // dependencies are : The list of all reactive values referenced inside of the fn code. Reactive values include props, state, and all the variables and functions declared directly inside your component body. 
@@ -10,6 +10,10 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password,setPassword] = useState("")
 
+  //useRef hook //useRef is a React Hook that lets you reference a value that’s not needed for rendering. //const ref = useRef(initialValue)
+  const passwordRef = useRef(null)
+
+
   const passwordGenerator =  useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -17,16 +21,27 @@ function App() {
     if(numberAllowed) str += "0123456789"
     if(charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
 
-      pass = str.charAt(char);
+      pass += str.charAt(char);
       
     }
 
     setPassword(pass)
 
-  },[length,numberAllowed,charAllowed,password])
+  },[length,numberAllowed,charAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    //passwordRef.current?.setSelectionRange(0,999)
+    //window //since we are working in react we have window object,if we are using next.js there it is server side rendering, and server doesnt have window object
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(() => {passwordGenerator()},[length,numberAllowed,charAllowed,passwordGenerator])  //useEffect is a React Hook that lets you synchronize a component with an external system.
+                            //useEffect(setup, dependencies?)
+
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -38,8 +53,11 @@ function App() {
           className ="outline-none w-full py-1 px-3"
           placeholder='Password'
           readOnly
+          ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+          <button
+          onClick={copyPasswordToClipboard}
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-gray-600">Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
@@ -49,8 +67,32 @@ function App() {
             max={100}
             value={length}
             className='cursor-pointer'
+            onChange={(e)=> {setLength(e.target.value)}}
             />
+            <label>Length: {length}</label>
           </div>
+        <div className="flex items-center gap-x-1">
+        <input
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={() => {
+                setNumberAllowed((prev) => !prev);
+            }}
+        />
+        <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+            <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="characterInput"
+                onChange={() => {
+                    setCharAllowed((prev) => !prev )
+                }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+        </div>  
         </div>
       </div>
       
